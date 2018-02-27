d2a6f1d6e13cbff09134e82fbe1f36569360c030
Euan Blackledge <euanblackledge@Euans-MacBook-Pro.local>
2018-02-27 22:58:28 +0000

    app setup complete

---
+++ App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import {
+  
+} from 'react-native'
+
+import MainContainer from './components/MainContainer'
+
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <MainContainer />
+    )
+  }
+}
+
---
+++ components/MainContainer.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import {
+  View,
+  Text,
+  StyleSheet,
+} from 'react-native'
+
+export default class MainContainer extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>BeBriefed App</Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+})
